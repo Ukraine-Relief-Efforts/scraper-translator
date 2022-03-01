fb12181adeb53a7c22549be34632f58c58fe86b2
Andrew Polaskey <apolaskey@gmail.com>
2022-03-01 08:00:10 -0500

    ES native modules for NodeJS

--- src/handlers/scraper-translation.js
+++ src/handlers/scraper-translation.js
@@ -1,7 +1,15 @@
-import { translate_object } from "../dynamodb";
-import { polandEn_v1 } from "../mocks/poland-en";
+import {translate_object} from "../dynamodb.js";
+import { polandEn_v1 } from "../mocks/poland-en.js";
 
-exports.scraperTranslatorHandler = async function (event, context) {
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { createRequire } from "module";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
+
+export const handler = async(event, context) => {
 
     console.log('Starting translator handler');
     console.log(event);
