698dc81403cfd7e4f7b3c6ff9e401fb12c8b9b2f
Blarp Technologies <blarptechnologies@icloud.com>
2022-03-01 22:29:29 -0700

    Dynamo GET and PUT functions are now GMO free, grass fed, and hormone free... oh and they also work and are lambda tested I guess

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,31 +1,69 @@
-import AWS from "aws-sdk";
+let AWS = require('aws-sdk');
+let dynamodb = new AWS.DynamoDB();
 
 AWS.config.update({
   region: "us-east-1",
 });
 
-const docClient = new AWS.DynamoDB.DocumentClient();
-
 export function getFromDynamo(country) {
     let params = {
         TableName: "TechForUkraine-CIG",
-        Key: {"country": country}
+        Key: { 
+            "country": {
+                S: country 
+            }
+        }
     };
-    return docClient.get(params).promise();
+
+    return new Promise((resolve, reject) => {
+        dynamodb.getItem(params, function(err, data) {
+            if (err) {
+                console.log("An error occurred GETting country from dynamo");
+                console.log(err, err.stack);
+                reject(err);
+            } else {
+                console.log("Successfully got", country, "from dynamo");
+                console.log(JSON.stringify(data));
+                resolve(data["Item"]);
+            }
+        });
+    });
 }
 
-export function putToDynamo(data) {
+export function putToDynamo(item) {
     var params = {
         TableName: "TechForUkraine-CIG",
-        Item: data
+        Item: item
     };
-    docClient.put(params, callback).promise();
+    
+    return new Promise((resolve, reject) => {
+        dynamodb.putItem(params, function(err, data) {
+            if (err) {
+                console.log("An error occurred PUTting country to dynamo");
+                console.log(err, err.stack);
+                reject(err);
+            } else {
+                console.log("Successfully put", item["country"]["S"], "to dynamo");
+                console.log(JSON.stringify(data));
+                resolve(data);
+            }
+        });
+    });
 }
 
-async function test() {
-    await putToDynamo({"country": "ree", "asdf": "foo"});
-    const data = await getFromDynamo("ree");
-    console.log(data);
-}
 
-test();
+/*
+
+    The 'payload' variable (line 61) is the "ThisIsMyTestPayload.json" file in /resources.
+
+*/
+// var testPayloadName = "THIS IS MY TEST PAYLOAD";
+// async function test() {
+//     await putToDynamo(payload);
+//     let data = await getFromDynamo(testPayloadName);
+//     data["country"]["S"] = testPayloadName + "UPDATED";
+//     await putToDynamo(data);
+//     console.log(data);
+// }
+
+// test();
